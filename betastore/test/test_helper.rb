282d10c198260367f4e87a00bfece539bfb59542
Sean Flaherty <sean.flaherty@gmail.com>
2014-07-30 13:08:22 -0400

    Added require 'capybara/rails'.

--- betastore/test/test_helper.rb
+++ betastore/test/test_helper.rb
@@ -11,3 +11,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+require 'capybara/rails'
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+end
